198310c786efda9b4eec69cdc4ade3aabaea1d49
Rakesh <Rakesh@Rakesh-MacBook-Air.local>
2015-10-23 16:17:57 +0530

    added phone number in script

--- a2b.js
+++ a2b.js
@@ -15,10 +15,10 @@
 				$('body').append("<button class='pay-using-A2B' style='cursor: pointer;'>Pay with A2B</button>");
 
 				$('.pay-using-A2B').click(function(){
-					if ($('.iframeA2B').length == 0) {
-						$('body').append("<div class='iframeA2B' style='display:none;background-color:rgba(0,0,0,0.4);position:absolute;top:0px;left:0px;width:100%;height:"+$(window).height()+"px;'>"+
+					if ($('#iframeA2B').length == 0) {
+						$('body').append("<div id='iframeA2B' style='display:none;background-color:rgba(0,0,0,0.4);position:absolute;top:0px;left:0px;width:100%;height:"+$(window).height()+"px;'>"+
 							"<div style='width:80%;min-width:300px;max-width:600px;margin:50px auto 0px;position:relative;border-radius:8px;box-shadow: 3px 11px 25px 2px;overflow:hidden;'>"+
-								"<iframe id='A2B-iframe' style='z-index: 999999;display: block;height: 600px;width: 600px;border-radious:10px;box-shadow:none;border:none;' src='http://a2b.zolome.com/#/?"+str+"'></iframe>"+
+								"<iframe id='A2B-iframe' onload='setFrameLoaded()' style='display: block;height: 600px;width: 600px;border-radious:10px;box-shadow:none;border:none;' src='http://a2b.zolome.com/#/?"+str+"'></iframe>"+
 								"<img class='close-A2B' src='' alt='X' style='cursor:pointer;position:absolute;top:10px;right:10px;'>"+
 							"</div>"+
 						"</div>");
@@ -26,14 +26,18 @@
 						$('.close-A2B').click(function(){
 							window.closePopup();
 						});
+
+						window.setFrameLoaded = function (argument) {
+							console.log("setFrameLoaded");
+						};
 					}
 
-					$('.iframeA2B').css("display", "block");
+					$('#iframeA2B').css("display", "block");
 					$('.token').remove();
 				});
 
 				window.closePopup = function (token) {
-					$('.iframeA2B').css("display", "none");
+					$('#iframeA2B').css("display", "none");
 					if (token) {
 						$('.pay-using-A2B').prop('disabled', true);
 						$('.pay-using-A2B').after('<p class=token>Your AtoB token is: <strong>' + token + '</strong></p>');
