3866deeed72313db2b5c5b80a7fb9b9cf7f71be3
Rakesh <Rakesh@Rakesh-MacBook-Air.local>
2015-10-29 11:59:31 +0530

    miner fix

--- a2b.js
+++ a2b.js
@@ -6,7 +6,7 @@
 			console.log(data.merchants);
 			for (var i = 0; i < data.merchants.length; i++) {
 				console.log(data.merchants[i].id);
-				$('#merchant-id-dropdown').append("<option value="+i+">"+data.merchants[i].id+"</option>");
+				$('#merchant-id-dropdown').append("<option value="+i+">"+data.merchants[i].merchantName+"</option>");
 			}
 		
 			$.ajax({
@@ -28,11 +28,11 @@
 							if ($('#amount').val() != '' && $('#merchant-name').val() != '') {
 								$('#iframeA2B').remove();
 								var selectedMerchant = data.merchants[$('#merchant-id-dropdown').val()];
-								str = str+'&amount='+$('#amount').val()+'&merchant_name='+$('#merchant-name').val()+'&merchant_secret='+btoa(selectedMerchant.secretKey)+'&merchant_id='+btoa(selectedMerchant.id);
+								str = str+'&amount='+$('#amount').val()+'&merchant_name='+selectedMerchant.merchantName+'&merchant_secret='+btoa(selectedMerchant.secretKey)+'&merchant_id='+btoa(selectedMerchant.id);
 								if ($('#iframeA2B').length == 0) {
 									$('#a2b-iframe-continer').append("<div id='iframeA2B' style='display:none;background-color:rgba(0,0,0,0.4);position:absolute;top:0px;left:0px;width:100%;height:"+$(window).height()+"px;'>"+
 										"<div style='width:80%;min-width:300px;max-width:600px;margin:50px auto 0px;position:relative;border-radius:8px;box-shadow: 3px 11px 25px 2px;overflow:hidden;'>"+
-											"<iframe id='A2B-iframe' onload='setFrameLoaded()' style='display: block;height: 500px;width: 600px;border-radious:10px;box-shadow:none;border:none;' src='http://a2b.zolome.com/#/?"+str+"'></iframe>"+
+											"<iframe id='A2B-iframe' onload='setFrameLoaded()' style='display: block;height: 500px;width: 600px;border-radious:10px;box-shadow:none;border:none;' src='http://localhost:8000/#/?"+str+"'></iframe>"+
 											"<img class='close-A2B' src='' alt='X' style='cursor:pointer;position:absolute;top:10px;right:10px;'>"+
 										"</div>"+
 									"</div>");
@@ -50,12 +50,19 @@
 								$('.token').remove();
 							}
 						});
+						// function onStorageEvent(storageEvent){
+
+						//     alert("storage event");
+						// }
+
+						// window.addEventListener('storage', onStorageEvent, false);
 
 						window.closePopup = function (token) {
 							$('#iframeA2B').css("display", "none");
 							if (token) {
 								$('.pay-using-A2B').prop('disabled', true);
 								$('.pay-using-A2B').after('<p class="token" style="margin: 8px;">Paid successfully, Your AtoB charge id is: <strong>' + token + '</strong></p>');
+								localStorage.removeItem('AtoBChargeId')
 							}
 						}
 			    	}
