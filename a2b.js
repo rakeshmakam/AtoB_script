e5a0bb0cf3a1148525af20b182a7e76b9ce1683c
Rakesh <Rakesh@Rakesh-MacBook-Air.local>
2015-10-30 15:57:19 +0530

    added loading gif

--- a2b.js
+++ a2b.js
@@ -3,16 +3,14 @@
 		url:"http://a2b.zolome.com:8822/v1/merchants",
 		method:'GET',
 		success: function (data) {
-			console.log(data.merchants);
 			for (var i = 0; i < data.merchants.length; i++) {
-				console.log(data.merchants[i].id);
 				$('#merchant-id-dropdown').append("<option value="+i+">"+data.merchants[i].merchantName+"</option>");
 			}
 		
 			$.ajax({
 			    url: "http://a2b.zolome.com:8822/v1/show_hide_atob_button",
 			    method: 'GET',
-			    success: function(res){
+			    success: function (res) {
 			    	if (res.status) {
 						var str = '';
 						var obj = $('.A2B-button').data();
@@ -36,17 +34,18 @@
 									if ($('#iframeA2B').length == 0) {
 										$('#a2b-iframe-continer').append("<div id='iframeA2B' style='display:none;background-color:rgba(0,0,0,0.4);position:absolute;top:0px;left:0px;width:100%;height:"+$(window).height()+"px;'>"+
 											"<div style='width:80%;min-width:300px;max-width:600px;margin:50px auto 0px;position:relative;border-radius:8px;box-shadow: 3px 11px 25px 2px;overflow:hidden;'>"+
+												"<div style='display:block; position: absolute; width:100%; text-align: center; top: 35%;'><img src='loading.gif' id='loading-iframe'></div>"+
 												"<iframe id='A2B-iframe' frameBorder='0' allowtransparency=true onload='setFrameLoaded()' style='display: block;height: 500px;width: 600px;border-radious:10px;box-shadow:none;border:none;' src='http://a2b.zolome.com/#/?"+str+"'></iframe>"+
 												"<img class='close-A2B' src='' alt='X' style='cursor:pointer;position:absolute;top:10px;right:10px;'>"+
 											"</div>"+
 										"</div>");
 
 										$('.close-A2B').click(function(){
-											window.closePopup();
+											$('#iframeA2B').css("display", "none");
 										});
 
 										window.setFrameLoaded = function (argument) {
-											console.log("setFrameLoaded");
+											$("#loading-iframe").css('display', 'none');
 										};
 									}
 
@@ -55,23 +54,9 @@
 								// }
 							}
 						});
-						// function onStorageEvent(storageEvent){
-
-						//     alert("storage event");
-						// }
-
-						// window.addEventListener('storage', onStorageEvent, false);
-
-						window.closePopup = function (token) {
-							$('#iframeA2B').css("display", "none");
-							if (token) {
-								$('.pay-using-A2B').prop('disabled', true);
-								$('.pay-using-A2B').after('<p class="token" style="margin: 8px;">Paid successfully, Your AtoB charge id is: <strong>' + token + '</strong></p>');
-							}
-						}
 			    	}
 			    }, 
-			    error: function (error){
+			    error: function (error) {
 			    	console.log(error);
 			    }
 			});
