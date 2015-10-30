001c496fa572842f6f14659121f0068a506d2ac7
Rakesh <Rakesh@Rakesh-MacBook-Air.local>
2015-10-30 19:41:59 +0530

    miner changes

--- a2b.js
+++ a2b.js
@@ -20,7 +20,7 @@
 						}
 
 						str = str.slice(0,-1);
-						$('#a2b-button-continer').append("<div style='text-align:center;'><button class='pay-using-A2B' style='cursor: pointer;background-color: #53A3CD;border: none;color: #fff;padding: 10px 20px;margin-top: 10px;'>Pay with A2B</button></div>");
+						$('#a2b-button-continer').append("<div style='text-align:center;'><button disabled='disabled' class='pay-using-A2B' style='cursor: pointer;background-color: #C5C5C5;border: none;color: #fff;padding: 10px 20px;margin-top: 10px;'>Pay with A2B</button></div>");
 
 						$('.pay-using-A2B').click(function(){
 							if ($('#amount').val() != '' && $('#merchant-name').val() != '') {
@@ -34,7 +34,7 @@
 									if ($('#iframeA2B').length == 0) {
 										$('#a2b-iframe-continer').append("<div id='iframeA2B' style='display:none;background-color:rgba(0,0,0,0.4);position:absolute;top:0px;left:0px;width:100%;height:"+$(window).height()+"px;'>"+
 											"<div style='width:80%;min-width:300px;max-width:600px;margin:50px auto 0px;position:relative;border-radius:8px;box-shadow: 3px 11px 25px 2px;overflow:hidden;'>"+
-												"<div style='display:block; position: absolute; width:100%; text-align: center; top: 35%;'><img src='loading.gif' id='loading-iframe'></div>"+
+												"<div style='display:block; position: absolute; width:100%; text-align: center; top: 35%;'><img src='loading.gif' style='width:80px;' id='loading-iframe'></div>"+
 												"<iframe id='A2B-iframe' frameBorder='0' allowtransparency=true onload='setFrameLoaded()' style='display: block;height: 500px;width: 600px;border-radious:10px;box-shadow:none;border:none;' src='http://a2b.zolome.com/#/?"+str+"'></iframe>"+
 												"<img class='close-A2B' src='' alt='X' style='cursor:pointer;position:absolute;top:10px;right:10px;'>"+
 											"</div>"+
@@ -54,6 +54,14 @@
 								// }
 							}
 						});
+
+						$('#amount').keyup(function () {
+							if ($('#amount').val() > 0){
+								$('.pay-using-A2B').removeAttr('disabled').css('background-color', '#53A3CD')
+							} else {
+								$('.pay-using-A2B').attr('disabled','disabled').css('background-color', '#C5C5C5');
+							}
+						})
 			    	}
 			    }, 
 			    error: function (error) {
